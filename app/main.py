74a3c9c6654bc4c17d4d4a32daf37760706abccb
Anton <80984465+Antongo22@users.noreply.github.com>
2025-06-08 14:38:25 +0300

    страница 404

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import os
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from pathlib import Path
 
 from app.routers import pdfs
@@ -15,6 +17,22 @@ app.mount("/static", StaticFiles(directory="app/static", html=True), name="stati
 
 # Templates
 templates = Jinja2Templates(directory="app/templates")
+
+# Обработчики ошибок
+@app.exception_handler(404)
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    if exc.status_code == 404:
+        return templates.TemplateResponse(
+            "404.html",
+            {"request": request},
+            status_code=404
+        )
+    return templates.TemplateResponse(
+        "404.html",
+        {"request": request, "detail": str(exc.detail)},
+        status_code=exc.status_code
+    )
 
 # Include routers
 app.include_router(pdfs.router)
